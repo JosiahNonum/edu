5ed86f17f92e43c005b066a3d923c05e6e3cb9fb
JosiahNonum <106886203+JosiahNonum@users.noreply.github.com>
2023-02-22 10:33:59 -0600

    Update FencingJDL.cpp

--- FencingJDL/FencingJDL.cpp
+++ FencingJDL/FencingJDL.cpp
@@ -2,7 +2,6 @@
 // COSC 2436
 // Programming Set 1
 // 
-// I've left a lot of the debugging stuff in the code
 
 #include<iostream>
 #include<fstream>
@@ -13,28 +12,21 @@ using namespace std;
 
 int main() {
 
-
 	int numberOfCases = 0;		// how many cases are to be computed
 	string numberOfCasesString;	// holds the string values of cases
 
-
 	// getting filename from the user
 	string filename;
-	// cout << "Enter the file name: ";
-	// cin >> filename;
-	filename = "fence.txt";
-
+	cout << "Enter the file name: ";
+	cin >> filename;
 
 	// opening file
 	string ch;
 	fstream fin(filename, fstream::in);
 
-
 	// getting how many cases there are from the file
 	getline(fin, numberOfCasesString);
 	numberOfCases = stoi(numberOfCasesString);
-	cout << numberOfCases << endl << endl << endl;
-
 
 	// this runs once for each case there is
 	for (int i = 0; i < numberOfCases; i++)
@@ -48,8 +40,6 @@ int main() {
 
 		rows = int(rowsColumns[0] - 48);
 		columns = int(rowsColumns[2] - 48);
-		//cout << rows << " " << columns;
-
 
 		bool firstRowFound = false;
 		int rowsToBeFenced = 0, firstRow = 0, lastRow = 0;
@@ -57,7 +47,6 @@ int main() {
 		string hold;
 
 		int firstColumn = 9999, lastColumn = 0;
-
 
 		// runs once for each row there is
 		for (int r = 0; r < rows; r++)
@@ -76,25 +65,21 @@ int main() {
 				{
 					firstColumnTemp = j;
 					firstColumnFound = true;
-					//cout << "firstRow: /n" << j << endl;
-
 					lastColumnTemp = j;
 				}
 				else if (hold[j] == 'x')
 				{
 					lastColumnTemp = j;
-					//cout << "lastRow: \n" << j << endl;
 				}
 
 				if (firstColumnTemp < firstColumn && firstColumnTemp != 9999)
 				{
 					firstColumn = firstColumnTemp;
-					//cout << "first column in row " << r << " is " << j << endl;
 				}
+
 				if (lastColumnTemp > lastColumn)
 				{
 					lastColumn = lastColumnTemp;
-					//cout << "last column in row " << r << " is " << j << endl;
 				}
 			}
 
@@ -103,33 +88,27 @@ int main() {
 			if (firstColumnFound && firstRowFound == false)
 			{
 				firstRow = r;
-				//cout << "first row is " << firstColumn << endl;
 				firstRowFound = true;
 			}
 			else if (firstColumnFound && firstRowFound == true)
 			{
 				lastRow = r;
-				//cout << "last row is " << lastColumn << endl;
 			}
-
 		}
-
 
 
 		// the number of rows and columns is just bottom minus top and right minus left
 		if (firstColumn == 9999)
 			firstColumn = 0;
-		ColumnsToBeFenced = (lastColumn - firstColumn);
-		//cout <<endl<< "last colomn is " << lastColumn << " first column is " << firstColumn;
 
-		rowsToBeFenced = (lastRow - firstRow);
-		//cout << endl << "last row is " << lastRow << " first row is " << firstRow;
+		// +1 in each case since the value is inclusize
+		ColumnsToBeFenced = (lastColumn - firstColumn + 1);
+		rowsToBeFenced = (lastRow - firstRow + 1);
 
-		ColumnsToBeFenced++;
-		rowsToBeFenced++;
-
-		cout << endl << "Case " << i + 1 << ": " << rowsToBeFenced << " " << ColumnsToBeFenced << endl;
+		// displaying the results
+		cout << endl << "Case " << i + 1 << ": " << rowsToBeFenced << " " << ColumnsToBeFenced;
 	}
+
 
 	// closing the file
 	fin.close();
